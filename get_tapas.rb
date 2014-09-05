d9c509ad8cba94014d16fee089e465960786c59a
mgarciaisaia <mgarciaisaia+github@gmail.com>
2014-09-05 02:29:48 -0300

    Do not fail if directory exists

--- get_tapas.rb
+++ get_tapas.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'date'
 require 'open-uri'
+require 'fileutils'
 
 theDay = Date.new(1945, 8, 29)
 today = Date.today
@@ -9,7 +10,7 @@ while(theDay <= today) do
   dirname = theDay.strftime("%Y")
   filename = theDay.strftime("%Y/%Y%m%d.jpg")
   url = theDay.strftime("http://tapas.clarin.com/tapa/%Y/%m/%d/%Y%m%d.jpg")
-  Dir.mkdir dirname
+  FileUtils::mkdir_p dirname
   File.open(filename, 'wb') do |fo|
     fo.write open(url).read
   end
