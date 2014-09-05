6ba9439814af9507cbaf957ad3434fd506eb17ba
mgarciaisaia <mgarciaisaia+github@gmail.com>
2014-09-05 02:28:01 -0300

    Create directory

--- get_tapas.rb
+++ get_tapas.rb
@@ -6,8 +6,10 @@ theDay = Date.new(1945, 8, 29)
 today = Date.today
 
 while(theDay <= today) do
+  dirname = theDay.strftime("%Y")
   filename = theDay.strftime("%Y/%Y%m%d.jpg")
   url = theDay.strftime("http://tapas.clarin.com/tapa/%Y/%m/%d/%Y%m%d.jpg")
+  Dir.mkdir dirname
   File.open(filename, 'wb') do |fo|
     fo.write open(url).read
   end
